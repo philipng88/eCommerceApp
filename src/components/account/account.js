0ca2609ce6d12c381d1344c51dd78644f6e9da47
philipng88 <philiplyng@gmail.com>
2019-06-01 17:21:49 -0600

    finished navbar feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions'
+import PurchaseHistory from './purchaseHistory';
+import AccountInformation from './accountInformation';
 
 class Account extends Component {
+    
+    componentDidMount() {
+        const navbarLinks = [
+            {
+                _id: 0,
+                title: 'Purchase History',
+                active: false,
+                component: <PurchaseHistory />
+            },
+            {
+                _id: 1,
+                title: 'Account Information',
+                active: true,
+                component: <AccountInformation /> 
+            }
+        ]
+        this.props.setHeaderLinks([])
+        this.props.setNavbarLinks(navbarLinks) 
+    }
+    
     renderContent() {
-        return <h1>content</h1> 
+        let jsx
+        if (this.props.navbarLinks) {
+            this.props.navbarLinks.forEach(link => {
+                if (link.active) {
+                    jsx = link.component
+                }
+            })
+        }
+        return jsx  
     }
     
     render() {
@@ -13,5 +45,12 @@ class Account extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    const { headerLinks, navbarLinks } = state.headerNavbar 
+    return { headerLinks, navbarLinks }
+}
+
+Account = connect(mapStateToProps, actions)(Account)  
 
 export default Account;
