3c0417df47ff99383b4cf148fe4eda7bec5b2799
philipng88 <philiplyng@gmail.com>
2019-06-05 17:01:39 -0600

    built review form component

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
+import PageTitle from '../pageTitle'
+import ReviewForm from './reviewForm'
 import { connect } from 'react-redux'
 import * as actions from '../../actions'
-import PageTitle from '../pageTitle'
 
 class Review extends Component {
     
     componentDidMount() {
-        this.props.setHeaderLinks([])
+        this.props.setHeaderLinks([]) 
         this.props.setNavbarLinks([]) 
+    }
+
+    onSubmit = (fields) => {
+        console.log('fields')
     }
     
     render() {
         return (
             <div className='review'>
                 <PageTitle className='review__page-title' title='Order Review' />  
+                <ReviewForm className='review__form' onSubmit={this.onSubmit} /> 
             </div>
         );
     }
