8c366a8b8df4e3b8dd72adc728e1b571bbf4faff
philipng88 <philiplyng@gmail.com>
2019-06-05 23:54:03 -0600

    built the information routes and container components payment and shipping

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,37 +5,47 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Router, Route, Switch } from "react-router-dom";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
+const createStoreWithMiddleware = applyMiddleware()(
+    compose(
+        (window.devToolsExtension ? window.devToolsExtension() : f => f)(
+            createStore
+        )
+    )
+);
 
 import "./style/main.scss";
 import Layout from "./components/layout";
-import history from './history'; 
-import SignIn from './components/auth/signin';
-import SignUp from './components/auth/signup'; 
-import Account from './components/account/account';
-import Shop from './components/shop/shop';
-import Icons from './helpers/icons';
-import Review from './components/order/review';
+import history from "./history";
+import SignIn from "./components/auth/signin";
+import SignUp from "./components/auth/signup";
+import Account from "./components/account/account";
+import Shop from "./components/shop/shop";
+import Icons from "./helpers/icons";
+import Review from "./components/order/review";
+import Shipping from "./components/information/shipping";
+import Payment from "./components/information/payment";
 
 function main() {
-  Icons() 
-  ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <Router history={history}> 
-        <Layout>
-          <Switch>
-            <Route exact path='/' component={SignIn} /> 
-            <Route path='/signin' component={SignIn} /> 
-            <Route path='/signup' component={SignUp} /> 
-            <Route path='/account' component={Account} /> 
-            <Route path='/shop' component={Shop} />
-            <Route path='/order/review' component={Review} />
-          </Switch>
-        </Layout> 
-      </Router>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    Icons();
+    ReactDOM.render(
+        <Provider store={createStoreWithMiddleware(reducers)}>
+            <Router history={history}>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={SignIn} />
+                        <Route path="/signin" component={SignIn} />
+                        <Route path="/signup" component={SignUp} />
+                        <Route path="/account" component={Account} />
+                        <Route path="/shop" component={Shop} />
+                        <Route path="/order/review" component={Review} />
+                        <Route path="/information/shipping" component={Shipping} />
+                        <Route path="/information/payment" component={Payment} />
+                    </Switch>
+                </Layout>
+            </Router>
+        </Provider>,
+        document.querySelector(".app-wrapper")
+    );
 }
 
 document.addEventListener("DOMContentLoaded", main);
--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -1,60 +1,73 @@
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
-import { FormButton } from '../formFields';
-import history from '../../history';
-import ReviewProducts from './reviewProducts';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { FormButton } from "../formFields";
+import history from "../../history";
+import ReviewProducts from "./reviewProducts";
 
 class ReviewForm extends Component {
     render() {
         const { className, handleSubmit, subtotal } = this.props;
-        let tax = Math.round((subtotal * .065) * 100) / 100 
-        let shipping = 0.00
+        let tax = Math.round(subtotal * 0.065 * 100) / 100;
+        let shipping = 0.0;
         return (
-            <form onSubmit={handleSubmit} className={`${className} review-form`}>
-                <div className='review-form__legend'>
-                    <div className='review-form__legend__name'>Name</div>
-                    <div className='review-form__legend__quantity'>Quantity</div>
-                    <div className='review-form__legend__price'>Price</div>
+            <form
+                onSubmit={handleSubmit}
+                className={`${className} review-form`}
+            >
+                <div className="review-form__legend">
+                    <div className="review-form__legend__name">Name</div>
+                    <div className="review-form__legend__quantity">
+                        Quantity
+                    </div>
+                    <div className="review-form__legend__price">Price</div>
                 </div>
-                <ReviewProducts className='review-form__products' />
-                <div className='review-form__line'></div>
-                <Field className='review-form__proceed'
-                onClick={() => history.push('/signin')}
-                type='submit'
-                title='Proceed to Checkout'
-                name='proceed'
-                component={FormButton}/>
-                <Field className='review-form__back'
-                onClick={() => history.push('/shop')}
-                type='button'
-                title='Back'
-                name='back'
-                short={true} 
-                component={FormButton}/>
-                <div className='review-form__details review-details'>
-                    <div className='review-details__subtotal review-detail'>
-                        <div className='review-detail__title'>Subtotal</div>
-                        <div className='review-detail__price'>${subtotal}</div>
+                <ReviewProducts className="review-form__products" />
+                <div className="review-form__line" />
+                <Field
+                    className="review-form__proceed"
+                    onClick={() => history.push("/information/shipping")}
+                    type="submit"
+                    title="Proceed to Checkout"
+                    name="proceed"
+                    component={FormButton}
+                />
+                <Field
+                    className="review-form__back"
+                    onClick={() => history.push("/shop")}
+                    type="button"
+                    title="Back"
+                    name="back"
+                    short={true}
+                    component={FormButton}
+                />
+                <div className="review-form__details review-details">
+                    <div className="review-details__subtotal review-detail">
+                        <div className="review-detail__title">Subtotal</div>
+                        <div className="review-detail__price">${subtotal}</div>
                     </div>
-                    <div className='review-details__subtotal review-detail'>
-                        <div className='review-detail__title'>Tax</div>
-                        <div className='review-detail__price'>${tax}</div>
+                    <div className="review-details__subtotal review-detail">
+                        <div className="review-detail__title">Tax</div>
+                        <div className="review-detail__price">${tax}</div>
                     </div>
-                    <div className='review-details__subtotal review-detail'>
-                        <div className='review-detail__title'>Shipping</div>
-                        <div className='review-detail__price'>${shipping}</div>
+                    <div className="review-details__subtotal review-detail">
+                        <div className="review-detail__title">Shipping</div>
+                        <div className="review-detail__price">${shipping}</div>
                     </div>
-                    <div className='review-details__total review-detail review-detail-green'>
-                        <div className='review-detail__title review-detail-green__title'>Total</div>
-                        <div className='review-detail__price review-detail-green__price'>${subtotal + tax + shipping}</div>
+                    <div className="review-details__total review-detail review-detail-green">
+                        <div className="review-detail__title review-detail-green__title">
+                            Total
+                        </div>
+                        <div className="review-detail__price review-detail-green__price">
+                            ${subtotal + tax + shipping}
+                        </div>
                     </div>
                 </div>
             </form>
-        )
+        );
     }
 }
 ReviewForm = reduxForm({
-    form: 'ReviewForm'
+    form: "ReviewForm"
 })(ReviewForm);
 
-export default ReviewForm
+export default ReviewForm;
