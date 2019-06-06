68ccfaaa36abe6b1c4ef571972d95c13b871d39f
philipng88 <philiplyng@gmail.com>
2019-06-06 14:08:28 -0600

    built payment form

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -1,13 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
+import { FormInput, FormButton } from '../formFields'
+import history from '../../history'
+import OrderSummary from './orderSummary'
 
 class PaymentForm extends Component {
     render() {
+        const { className, handleSubmit } = this.props 
         return (
-            <div>
-                Payment Information Form 
-            </div>
+            <form onSubmit={handleSubmit} className={`${className} payment-form`}>
+                <Field className='payment-form__name' 
+                type='name'
+                title='Name on Credit Card'
+                placeholder='Name'
+                name='name'
+                component={FormInput}
+                /> 
+                <Field className='payment-form__card' 
+                type='card'
+                title='Credit Card Number'
+                placeholder='____-____-____-____'
+                name='card'
+                component={FormInput}
+                />   
+                <Field className='payment-form__expiration'
+                type='expiration'
+                title='Expiration Date'
+                placeholder='__/__'
+                name='expiration'
+                component={FormInput}
+                />
+                <Field className='payment-form__ccv'
+                type='ccv'
+                title='CCV'
+                placeholder='CCV'
+                name='ccv'
+                component={FormInput}
+                />
+                <div className='payment-form__line'></div>
+                <Field className='payment-form__pay-complete'
+                // onClick={() => history.push()}
+                type='submit'
+                title='Pay & Complete'
+                name='pay-complete'
+                component={FormButton}
+                />
+                <Field className='payment-form__back'
+                onClick={() => history.push('/information/shipping')}
+                type='button'
+                title='Back'
+                name='back'
+                short={true} 
+                component={FormButton}
+                />
+                <OrderSummary className='payment-form__order-summary' />
+            </form>
         );
     }
 }
-
-export default PaymentForm;
+PaymentForm = reduxForm({
+    form: 'PaymentForm'
+})(PaymentForm)
+export default PaymentForm
